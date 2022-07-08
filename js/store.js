c58ab6e90fa2f304b54ddd2bf4be235a9c49fcd9
Casaniv <casaniv2118@gmail.com>
2022-07-08 07:42:48 -0500

    Update sheets

--- js/store.js
+++ js/store.js
@@ -11,7 +11,7 @@ const detalles = document.getElementById('detalles')
 
 let carrito = [];
 let buttonDOM = [];
-const stringURLs = {
+stringURLs = {
     inventario: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRxOxuUOBEa_H45AUTOJD8LeRTNgBj4Ac-cr1F6oKlTFw2R25cIa0BIQEdRwO6TTqegxFdxQksEkZNX/pub?gid=0&single=true&output=csv",
     clients: ""
 };
